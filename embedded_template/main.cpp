b8e3569541799a9eee498a28a03372f33ff043bc
Sebastian Michel <sebastian.michel96@gmail.com>
2023-12-29 11:04:30 +0100

    don't optimize counts (for release build)

--- embedded_template/main.cpp
+++ embedded_template/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     auto now_steady = std::chrono::steady_clock::now();
 
-    auto new_system = std::chrono::system_clock::now();
+    volatile auto counts = now_steady.time_since_epoch().count();
 
     return 0;
 }
