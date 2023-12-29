149d0353cd15c4067035c103ea22d1a02f26bd82
Sebastian Michel <sebastian.michel96@gmail.com>
2023-12-29 11:23:52 +0100

    removed all device specific code from main branch

--- flags_architecture.cmake
+++ flags_architecture.cmake
@@ -1,23 +1,12 @@
-# set EMBEDDED_TEMPLATE_DEVICE in cache
-# can be overwritten by user (e.g. in cmake-gui or by editing CMakeCache.txt)
-set(EMBEDDED_TEMPLATE_DEVICE STM32H723xx CACHE STRING "Device used for embedded_template")
+add_library(flags_mcu INTERFACE)
 
-if (EMBEDDED_TEMPLATE_DEVICE STREQUAL STM32H723xx)
-    add_library(flags_mcu INTERFACE)
+set(EMBEDDED_TEMPLATE_ARCH_FLAGS
+        --specs=nano.specs
+        -mthumb
+)
 
-    set(EMBEDDED_TEMPLATE_ARCH_FLAGS
-            --specs=nano.specs
-            --specs=nosys.specs
-            -mthumb
-            -mcpu=cortex-m7
-            -mfloat-abi=hard
-    )
+target_compile_options(flags_mcu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS})
+target_link_options(flags_mcu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS})
 
-    target_compile_options(flags_mcu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS})
-    target_link_options(flags_mcu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS})
-
-    # make all following targets use the architecture specific compiler and linker flags
-    link_libraries(flags_mcu)
-else ()
-    message(FATAL_ERROR "Unsupported device: ${EMBEDDED_TEMPLATE_DEVICE}")
-endif ()
+# make all following targets use the architecture specific compiler and linker flags
+link_libraries(flags_mcu)
