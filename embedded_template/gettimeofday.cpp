43c7f1780996e0e24248047d7f1107dd5770a257
Sebastian Michel <sebastian.michel96@gmail.com>
2023-12-30 20:59:54 +0100

    made variables atomic, because they are most likely updated from an interrupt

--- embedded_template/gettimeofday.cpp
+++ embedded_template/gettimeofday.cpp
@@ -1,24 +1,28 @@
 #include "gettimeofday.h"
 #include <chrono>
+#include <atomic>
 
 namespace
 {
-    using TSeconds = decltype(std::declval<timeval>().tv_sec);
+    // using TSeconds = decltype(std::declval<timeval>().tv_sec);
+    // not using decltype because it's internally a long long (int64_t), which is not compatible with std::atomic on the cortex m platform
+    using TSeconds = std::uint32_t; //  4294967295 seconds = 136,1 years -> max uptime of 136 years is enough
     using TMicroseconds = decltype(std::declval<timeval>().tv_usec);
 
-    static inline TSeconds seconds_passed{0};
-    static inline TMicroseconds microseconds_passed{0};
+    // updated from the timer interrupt, so the variables need to be atomic
+    inline std::atomic<TSeconds> seconds_passed{0};
+    inline std::atomic<TMicroseconds> microseconds_passed{0};
 }
 
 
 extern "C" {
-int _gettimeofday(timeval* ptimeval,
-                  [[maybe_unused]] void* ptimezone)
-{
-    ptimeval->tv_sec = seconds_passed;
-    ptimeval->tv_usec = microseconds_passed;
-    return 0;
-}
+    int _gettimeofday(timeval* ptimeval,
+                      [[maybe_unused]] void* ptimezone)
+    {
+        ptimeval->tv_sec = seconds_passed;
+        ptimeval->tv_usec = microseconds_passed;
+        return 0;
+    }
 }
 
 namespace EmbeddedTemplate
@@ -26,9 +30,12 @@ namespace EmbeddedTemplate
     void on_microseconds_passed(const TMicroseconds microseconds)
     {
         static constexpr auto microseconds_per_second = 1000 * 1000;
-        microseconds_passed += microseconds;
 
-        if (microseconds_passed >= microseconds_per_second)
+        // use a non atomic variable to store the result, because re-reading the atomic variable later in the
+        // if statement is more expensive
+        const auto result = microseconds_passed += microseconds;
+
+        if (result >= microseconds_per_second)
         {
             seconds_passed += 1;
             microseconds_passed -= microseconds_per_second;
