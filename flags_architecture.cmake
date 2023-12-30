893fb777e35ed44df705cdecccc6addff8bb3744
Sebastian Michel <sebastian.michel96@gmail.com>
2023-12-30 20:26:48 +0100

    Added STM32G474xx specific submodules / cmake code

--- flags_architecture.cmake
+++ flags_architecture.cmake
@@ -10,3 +10,18 @@ target_link_options(flags_mcu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS})
 
 # make all following targets use the architecture specific compiler and linker flags
 link_libraries(flags_mcu)
+
+if (EMBEDDED_TEMPLATE_DEVICE STREQUAL STM32G474xx)
+    add_library(flags_cpu INTERFACE)
+
+    set(EMBEDDED_TEMPLATE_ARCH_FLAGS_CPU
+            -mcpu=cortex-m4
+            -mfloat-abi=hard
+    )
+
+    target_compile_options(flags_cpu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS_CPU})
+    target_link_options(flags_cpu INTERFACE ${EMBEDDED_TEMPLATE_ARCH_FLAGS_CPU})
+
+    # make all following targets use the architecture specific compiler and linker flags
+    link_libraries(flags_cpu)
+endif ()
